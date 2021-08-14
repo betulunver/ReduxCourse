9ba14c0f9e2462a818475b2313266942914832bd
Betul UNVER <betul.unver@bilgeadam.com>
2021-08-15 00:46:29 +0300

    create new app and components

---
+++ app/my-app/src/components/News.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import NewsItem from "./NewsItem";
+class News extends React.Component{
+    render(){
+        return (
+            <div>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+            </div>
+        )
+    }
+}
+export default News;
---
+++ app/my-app/src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NewsItem extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello React</h1>
+                <p>Loremipsum</p>
+            </div>
+        )
+    }
+}
+export default NewsItem;
